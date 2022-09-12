a5b58dda1271cbae093c97c5ba2a51b5f02b734d
Mayank <mayank2157@gmail.com>
2022-09-12 11:58:18 +0530

    remove duplicated

--- linkedlist_basic/removeduplicatesfromsortedlist.cpp
+++ linkedlist_basic/removeduplicatesfromsortedlist.cpp
@@ -69,3 +69,24 @@ int main(){
     
 };
 
+
+// Leetcode Solution
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *curr = head;
+        while(curr != NULL && curr -> next != NULL){
+            if(curr -> val == curr -> next -> val){
+                ListNode *temp = curr -> next;
+                curr -> next = curr -> next -> next;
+                delete(temp);
+            }
+            else{
+                curr =  curr -> next;
+                
+            }
+        };
+        
+        return head;
+    }
+};
