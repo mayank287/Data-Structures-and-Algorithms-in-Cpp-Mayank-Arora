c331196598e5cd60db401127838b788cd46a4c61
Mayank <mayank2157@gmail.com>
2022-09-12 11:56:46 +0530

    remove duplicated

---
+++ linkedlist_basic/removeduplicatesfromsortedlist.cpp
@@ -0,0 +1,71 @@
+
+#include <iostream>
+using namespace std;
+
+struct Node{
+  int data;
+  Node *next;
+  Node (int x){
+    data = x;
+    next= NULL;
+  };
+  
+    
+};
+
+
+
+void RemoveDuplicate(Node *head){
+    Node *curr = head;
+    while(curr != NULL &&  curr -> next != NULL ){
+        if(curr -> data == curr -> next -> data){
+            Node *temp = curr -> next;
+            curr -> next = curr -> next -> next;
+            delete(temp);
+            
+        }
+        else{
+            curr = curr -> next;
+            
+        }
+    };
+};
+
+
+
+
+void printll(Node *head){
+    if(head == NULL){
+        return;
+    };
+    
+    
+    cout << head -> data << " ";
+    printll(head -> next);
+    
+};
+
+
+
+
+int main(){
+    Node *head = new Node(10);
+    head ->  next = new Node(20);
+    head -> next -> next  = new Node(20);
+    head -> next -> next  -> next = new Node(30);
+    head -> next -> next -> next -> next = new Node(30);
+    head -> next -> next -> next -> next  -> next = new Node(30);
+    head -> next -> next -> next ->next  -> next -> next = new Node(50);
+  
+  
+      RemoveDuplicate(head);
+      
+    printll(head);
+    
+    
+    return 0;
+    
+    
+    
+};
+
