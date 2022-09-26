c1d94c01b140646c968f122a16a09f4359c08180
Mayank <mayank2157@gmail.com>
2022-09-26 17:44:50 +0530

    c++stlstart

---
+++ C++ STL/Iterators/Iterators1.cpp
@@ -0,0 +1,27 @@
+// Iterators Program 1
+// Iterators are Like Pointers
+
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v ={10,20,30,40,50};
+    vector<int> :: iterator i = v.begin();
+    cout << *i << " ";
+    i++;
+    cout << *i << " ";
+    i = v.end();
+    i--;
+    cout << *i << " ";
+    return 0;
+    
+    
+
+    return 0;
+}
+
+
+// O/P :- 10,20,50
---
+++ C++ STL/Iterators/iterators2.cpp
@@ -0,0 +1,23 @@
+ // Iterators Program 1
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v ={10,20,30,40,50};
+    vector<int> :: iterator i = v.begin();
+    i = next(i);
+    cout << *i << " ";
+    i  = next(i,2);
+    cout << *i << " ";
+    i = prev(i);
+    
+    cout << *i << " ";
+    return 0;
+    
+    
+
+    return 0;
+}
