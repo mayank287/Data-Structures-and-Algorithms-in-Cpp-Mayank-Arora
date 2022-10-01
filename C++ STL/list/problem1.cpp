0bb21698128c3d562bb120dfaa437fe47e5aa22b
Mayank <mayank2157@gmail.com>
2022-10-01 22:09:42 +0530

    listproblem

---
+++ C++ STL/list/problem1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<vector>
+#include<list>
+#include<algorithm>
+using namespace std;
+
+list<int> l;
+ 
+void insert(int x){
+    l.push_back(x);
+    
+}
+
+void print(){
+    for(int x : l ){
+        cout << x << " ";
+        cout << endl;
+        
+    }
+}
+
+
+
+void replace(int x, vector<int> &seq){
+    auto it = find(l.begin(),l.end(),x);
+    if(it == l.end()){
+        return;
+        
+    }
+    
+    it = l.erase(it);
+    l.insert(it,seq.begin(),seq.end());
+    
+}
+
+
+
+int main()
+{
+   insert(10);
+   insert(20);
+   insert(30);
+   
+   
+   vector<int> seq ={50,60,70};
+   
+   
+   replace(20,seq);
+   print();
+   
+
+
+   
+    return 0;
+}
