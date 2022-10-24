acfe073ffbbc1d57e01247ab34e3aadd9ea26308
Mayank Arora <83607303+mayank287@users.noreply.github.com>
2022-10-24 11:10:43 +0530

    Maths Concepts

---
+++ 012 - Maths/PrintallDivisors(naive).cpp
@@ -0,0 +1,25 @@
+// Print all Divisors
+//  Time  Complexity theta(n)
+#include <iostream>
+using namespace std;
+
+void printDivisord(int n)
+{
+    for(int i = 1; i <= n;i++){
+        if(n % i == 0) 
+        {
+            cout << i << endl;
+            
+        }
+    }
+}
+
+int main()
+{
+  
+  
+  printDivisord(15);
+   
+  
+    return 0;
+}
