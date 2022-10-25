a59b3ddf50e5912b1c271a47237e88e472fdbe88
Mayank Arora <83607303+mayank287@users.noreply.github.com>
2022-10-25 12:41:38 +0530

    Some More Maths Concepts

---
+++ 012 - Maths/sieve.cpp
@@ -0,0 +1,46 @@
+// sieve 
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+void sieve(int n)
+{
+    vector<bool> isPrime(n + 1, true );
+    for(int i = 2; i * i <= n; i++)
+    {
+        if(isPrime[i])
+        {
+            for(int j = 2 * i; j <= n; j += i)
+            {
+                isPrime[j] = false;
+                
+            }
+        }
+    }
+    
+    for(int i = 2; i <=n; i++)
+    {
+        if(isPrime[i])
+        {
+            cout << i << endl;
+            
+        }
+    }
+}
+
+
+
+
+int main()
+{ 
+   int n;
+   cin >> n;
+   
+    
+   
+    sieve(n);
+   
+  
+    return 0;
+}
