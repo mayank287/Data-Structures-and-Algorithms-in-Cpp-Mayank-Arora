fa637c468fca2c356254111b035e45cc62b3bc00
Your Name <mayank2157@gmail.com>
2022-11-19 01:28:33 +0530

    Strings Algos for competutve programming

---
+++ 017 - Strings/LeftMostNonRepeatingChar(Naive).cpp
@@ -0,0 +1,41 @@
+// Left Most Non Repeating Char(Naive)
+// Time Complexuty O(n2)
+#include <iostream>
+using namespace std;
+
+int nonRep(string &str)
+{
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        bool flag = false;
+        for(int j = 0; j < str.length(); j++)
+        {
+            if(i != j && str[i] == str[j])
+            {
+                flag = true;
+                break;
+            }
+        }
+        
+        
+        if(flag == false) return i;
+        
+        
+        
+        }
+    return -1;
+    
+}
+
+
+
+
+int main()
+{
+   string str = "abcbca";
+   cout << nonRep(str);
+   
+
+    return 0;
+}
