7ac94bfe0c95cb601c48b6282cdb3d13af8cf646
Your Name <mayank2157@gmail.com>
2022-12-27 23:31:05 +0530

    Dp

---
+++ 029 - DynamicProgramming/FibousingMemoization.cpp
@@ -0,0 +1,32 @@
+// Fibo Series Using Memoization Method of Dynamic Programming 
+// Time Complexty theta(n)
+#include<iostream>
+#include<string.h>
+using namespace std;
+int memo[1000];
+
+int fib(int n)
+{
+  if(memo[n] == -1)
+  {
+      int res;
+      if(n == 0 || n == 1) return n;
+      else
+      {
+        res = fib(n - 1)  + fib(n - 2);    
+      }
+      
+      memo[n] = res; 
+  }
+  return memo[n];
+  
+  
+}
+
+int main()
+{
+    int n = 5;
+    memset(memo,-1,sizeof(memo));
+    
+    cout << fib(10) << endl;
+}
