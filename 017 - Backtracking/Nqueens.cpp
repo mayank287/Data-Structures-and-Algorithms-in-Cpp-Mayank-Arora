f1a8d117740a2b44311e12bac78bf6e419b28570
Mayank <mayank2157@gmail.com>
2023-01-09 14:11:53 +0530

    nqueens modification

--- 017 - Backtracking/Nqueens.cpp
+++ 017 - Backtracking/Nqueens.cpp
@@ -142,3 +142,61 @@ public:
         return result;
     }
 }; 
+
+// Leetcode Solution
+class Solution {
+public:
+  bool is_valid(vector<string> board,int row,int col)
+  {
+      for(int i = row; i >= 0; i--)
+      {
+          if(board[i][col] == 'Q') return false;
+
+      } 
+       
+       for(int i = row,j = col; i>=0 && j>=0; i--,j--)
+       {
+           if(board[i][j] == 'Q') return false;
+       }
+       for(int i = row,j = col; i>= 0&&j < board.size(); i--,j++)
+       {
+           if(board[i][j] == 'Q') return false;
+       }
+
+       return true;
+
+  }
+   
+   void solve(int row,vector<string>&board,vector<vector<string>> &ans)
+   {
+       if(row == board.size())
+       {
+           ans.push_back(board);
+           return;
+       }
+
+     for(int i = 0; i < board.size(); i++)
+     {
+         if(is_valid(board,row,i))
+         {
+             board[row][i] = 'Q';
+             solve(row + 1,board,ans);
+             board[row][i] = '.';
+         }
+     }
+
+
+   }
+ 
+
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n,'.'));
+        vector<vector<string>> ans;
+        solve(0,board,ans);
+        return ans;
+
+
+
+    }
+};
