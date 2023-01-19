50792be8977828bf089368dc0419d635ecdd1fc1
Mayank Arora <83607303+mayank287@users.noreply.github.com>
2023-01-19 11:38:26 +0530

    Update RangeQueryOperationsUsingprefixsum.cpp

--- 025 - SegmentTree/RangeQueryOperationsUsingprefixsum.cpp
+++ 025 - SegmentTree/RangeQueryOperationsUsingprefixsum.cpp
@@ -5,30 +5,48 @@
 #include <iostream>
 using namespace std;
 
-int getSum(int arr[], int qs, int qe)
+int getSum(int pre_sum[], int qs, int qe)
 {
-	int sum = 0;
-
-	for(int i = qs; i <= qe; i++)
-		sum = sum + arr[i];
-
-	return sum;
+	if(qs == 0)
+	    return pre_sum[qe];
+	    
+	else    
+	    return pre_sum[qe] - pre_sum[qs - 1];
 }
 
-void update(int arr[], int i, int newVal)
+void update(int pre_sum[], int arr[], int i, int newVal, int n)
 {
-	arr[i] = newVal;
-}
+	int diff = newVal - arr[i];
 	
+	for(int j = i; j < n; j++)
+	    pre_sum[j] += diff;
+}
+
+void initialize(int pre_sum[], int arr[], int n)
+{
+    pre_sum[0] = arr[0];
+    
+    for(int i = 1; i < n; i++)
+    {
+        pre_sum[i] = pre_sum[i - 1] + arr[i];
+    }
+}	
 	
 int main() {
 	int arr[] = {10, 20, 30, 40, 50}, n= 5;
-
-	cout << getSum(arr, 0, 2) << " ";
-	cout << getSum(arr, 1, 3) << " ";
-	update(arr, 1, 60);
-	cout << getSum(arr, 0, 2) << " ";
-	cout << getSum(arr, 1, 3) << " ";
+    
+    int pre_sum[n];
+        
+    initialize(pre_sum, arr, n);
+    
+    
+	cout << getSum(pre_sum, 0, 2) << " ";
+	cout << getSum(pre_sum, 1, 3) << " ";
+	
+	update(pre_sum, arr, 1, 60, n);
+	
+	cout << getSum(pre_sum, 0, 2) << " ";
+	cout << getSum(pre_sum, 1, 3) << " ";
 		
 	return 0;
 }
